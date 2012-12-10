0a4c5a3eac6249d8d09cf6895f5e16107ecd9cf4
cbodden <cesar@pissedoffadmins.com>
2012-12-10 13:52:22 -0500

    Syntax changes.

--- pushbutton_counter.ino
+++ pushbutton_counter.ino
@@ -6,30 +6,30 @@ int previousState = 0;
 
 void setup(){
   //switched pinMode1 to pinMode11 due to TX/RX
-  pinMode(11, OUTPUT); 
-  pinMode(2, OUTPUT); 
-  pinMode(3, OUTPUT); 
-  pinMode(4, OUTPUT); 
-  pinMode(5, OUTPUT); 
-  pinMode(6, OUTPUT); 
-  pinMode(7, OUTPUT); 
-  pinMode(8, OUTPUT); 
+  pinMode(11, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(switchPin, INPUT);
   //Serial.begin(9600);
-  digitalWrite(11, HIGH); 
-  digitalWrite(2, HIGH); 
-  digitalWrite(3, HIGH); 
-  digitalWrite(4, HIGH); 
-  digitalWrite(5, HIGH); 
-  digitalWrite(6, HIGH); 
-  digitalWrite(7, HIGH); 
-  digitalWrite(8, HIGH); 
+  digitalWrite(11, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
   digitalWrite(9, HIGH);
 }
 
 void loop(){
-  val = digitalRead(switchPin); 
+  val = digitalRead(switchPin);
   updateDisplay(currentState);
   if (val == HIGH){
     delay(500);
@@ -49,225 +49,224 @@ void loop(){
 
 void updateDisplay(int currentState){
   if(currentState == 15){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(11, LOW); 
-    digitalWrite(5, LOW); 
-    digitalWrite(6, LOW); 
+    digitalWrite(11, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
     digitalWrite(7, LOW);
   }
   else if(currentState == 14){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(11, LOW); 
-    digitalWrite(4, LOW); 
-    digitalWrite(5, LOW); 
-    digitalWrite(6, LOW); 
+    digitalWrite(11, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
     digitalWrite(7, LOW);
   }
   else if(currentState == 13){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(2, LOW); 
-    digitalWrite(3, LOW); 
-    digitalWrite(4, LOW); 
-    digitalWrite(5, LOW); 
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
     digitalWrite(7, LOW);
   }
   else if(currentState == 12){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(11, LOW); 
-    digitalWrite(4, LOW); 
-    digitalWrite(5, LOW); 
+    digitalWrite(11, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
     digitalWrite(6, LOW);
   }
   else if(currentState == 11){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(3, LOW); 
-    digitalWrite(4, LOW); 
-    digitalWrite(5, LOW); 
-    digitalWrite(6, LOW); 
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
     digitalWrite(7, LOW);
   }
   else if(currentState == 10){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(11, LOW); 
-    digitalWrite(2, LOW); 
-    digitalWrite(3, LOW); 
-    digitalWrite(5, LOW); 
-    digitalWrite(6, LOW); 
+    digitalWrite(11, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
     digitalWrite(7, LOW);
   }
   else if(currentState == 9){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(11, LOW); 
-    digitalWrite(2, LOW); 
-    digitalWrite(3, LOW); 
-    digitalWrite(6, LOW); 
+    digitalWrite(11, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(6, LOW);
     digitalWrite(7, LOW);
   }
   else if(currentState == 8){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(11, LOW); 
-    digitalWrite(2, LOW); 
-    digitalWrite(3, LOW); 
-    digitalWrite(4, LOW); 
-    digitalWrite(5, LOW); 
-    digitalWrite(6, LOW); 
+    digitalWrite(11, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
     digitalWrite(7, LOW);
   }
   else if(currentState == 7){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(11, LOW); 
-    digitalWrite(2, LOW); 
+    digitalWrite(11, LOW);
+    digitalWrite(2, LOW);
     digitalWrite(3, LOW);
   }
   else if(currentState == 6){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(11, LOW); 
-    digitalWrite(3, LOW); 
-    digitalWrite(4, LOW); 
-    digitalWrite(5, LOW); 
-    digitalWrite(6, LOW); 
+    digitalWrite(11, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
     digitalWrite(7, LOW);
   }
   else if(currentState == 5){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(11, LOW); 
-    digitalWrite(3, LOW); 
-    digitalWrite(4, LOW); 
-    digitalWrite(6, LOW); 
+    digitalWrite(11, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(6, LOW);
     digitalWrite(7, LOW);
   }
   else if(currentState == 4){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(2, LOW); 
-    digitalWrite(3, LOW); 
-    digitalWrite(6, LOW); 
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(6, LOW);
     digitalWrite(7, LOW);
   }
   else if(currentState == 3){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(11, LOW); 
-    digitalWrite(2, LOW); 
-    digitalWrite(3, LOW); 
-    digitalWrite(4, LOW); 
+    digitalWrite(11, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
     digitalWrite(7, LOW);
   }
   else if(currentState == 2){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(11, LOW); 
-    digitalWrite(2, LOW); 
-    digitalWrite(4, LOW); 
-    digitalWrite(5, LOW); 
+    digitalWrite(11, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
     digitalWrite(7, LOW);
   }
   else if(currentState == 1){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(2, LOW); 
+    digitalWrite(2, LOW);
     digitalWrite(3, LOW);
   }
   else if(currentState == 0){
-    digitalWrite(11, HIGH); 
-    digitalWrite(2, HIGH); 
-    digitalWrite(3, HIGH); 
-    digitalWrite(4, HIGH); 
-    digitalWrite(5, HIGH); 
-    digitalWrite(6, HIGH); 
+    digitalWrite(11, HIGH);
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, HIGH);
     digitalWrite(7, HIGH);
-    digitalWrite(11, LOW); 
-    digitalWrite(2, LOW); 
-    digitalWrite(3, LOW); 
-    digitalWrite(4, LOW); 
-    digitalWrite(5, LOW); 
+    digitalWrite(11, LOW);
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
     digitalWrite(6, LOW);
   }
 }
-
